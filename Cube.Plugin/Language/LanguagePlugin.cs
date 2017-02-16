5c873752ca0b5e75495da82cd6474be5c2734566
MosesZhu <Moses.Zhu@qisda.com>
2017-02-16 09:22:01 +0800

    add Plugin project

---
+++ Cube.Plugin/Language/LanguagePlugin.cs
@@ -0,0 +1,13 @@
+﻿using Cube.Plugin.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube.Plugin.Language
+{
+    public class LanguagePlugin : IPlugin
+    {
+    }
+}
