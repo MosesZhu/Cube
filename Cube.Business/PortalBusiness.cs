355d8f1f81f97e1d9ddc0cb102bb7123157cf0d5
MosesZhu <Moses.Zhu@qisda.com>
2017-02-23 13:13:27 +0800

    daily commit

---
+++ Cube.Business/PortalBusiness.cs
@@ -0,0 +1,39 @@
+﻿using Cube.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube.Business
+{
+    public class PortalBusiness
+    {
+        public List<MenuDTO> getMenu()
+        {
+            //Test
+            List<MenuDTO> menuList = new List<MenuDTO>();
+            MenuDTO Domain_MFG = new MenuDTO();
+            Domain_MFG.ID = "xxx";
+            Domain_MFG.Code = "MFG";
+            Domain_MFG.LanguageID = "lang_MFG";
+
+            MenuDTO System_QML = new MenuDTO();
+            System_QML.ID = "xxx_1";
+            System_QML.Code = "QML";
+            Domain_MFG.LanguageID = "lang_QML";
+
+            MenuDTO Function_QML_1 = new MenuDTO();
+            Function_QML_1.ID = "xxx_1_1";
+            Function_QML_1.Code = "QML_BASE_DATA_MAINTAIN";
+            Function_QML_1.LanguageID = "lang_menu_aaa";
+            Function_QML_1.Url = "http://localhost:28937/Forms/TestDataMaintainForm.aspx";
+
+            System_QML.SubMenuList.Add(Function_QML_1);
+            Domain_MFG.SubMenuList.Add(System_QML);
+            menuList.Add(Domain_MFG);
+
+            return menuList;
+        }
+    }
+}
--- Cube.Web/PortalService.asmx.cs
+++ Cube.Web/PortalService.asmx.cs
@@ -1,4 +1,5 @@
 ﻿using Cube.Base;
+using Cube.Business;
 using Cube.DTO;
 using System;
 using System.Collections.Generic;
@@ -14,31 +15,9 @@ namespace Cube.Web
         [WebMethod]
         public ResultDTO getMenu()
         {
-            ResultDTO result = new ResultDTO();
-            //Test
-            List<MenuDTO> menuList = new List<MenuDTO>();
-            MenuDTO Domain_MFG = new MenuDTO();
-            Domain_MFG.ID = "xxx";
-            Domain_MFG.Code = "MFG";
-            Domain_MFG.LanguageID = "lang_MFG";
-
-            MenuDTO System_QML = new MenuDTO();
-            System_QML.ID = "xxx_1";
-            System_QML.Code = "QML";
-            Domain_MFG.LanguageID = "lang_QML";
-
-            MenuDTO Function_QML_1 = new MenuDTO();
-            Function_QML_1.ID = "xxx_1_1";
-            Function_QML_1.Code = "QML_BASE_DATA_MAINTAIN";
-            Function_QML_1.LanguageID = "lang_menu_aaa";
-            Function_QML_1.Url = "http://localhost:28937/Forms/TestDataMaintainForm.aspx";
-
-            System_QML.SubMenuList.Add(Function_QML_1);
-            Domain_MFG.SubMenuList.Add(System_QML);
-            menuList.Add(Domain_MFG);
-
-            result.data = menuList;
-            //Test
+            ResultDTO result = new ResultDTO();            
+            
+            result.data = (new PortalBusiness()).getMenu();            
 
             return result;
         }
